f242008b096cd45be01123bcebcc616f5976468c
knox <knoxfighter@asdff45.de>
2025-10-05 21:16:57 +0200

    fix scrolling and add CleanConsoleAction to serve command

--- src/main/kotlin/com/dioxuslabs/dioxus/runconfig/TerminalConsoleView.kt
+++ src/main/kotlin/com/dioxuslabs/dioxus/runconfig/TerminalConsoleView.kt
@@ -1,10 +1,16 @@
+import com.intellij.execution.ExecutionBundle
 import com.intellij.execution.filters.Filter
 import com.intellij.execution.process.ProcessHandler
 import com.intellij.execution.ui.ConsoleView
 import com.intellij.execution.ui.ConsoleViewContentType
+import com.intellij.icons.AllIcons
+import com.intellij.openapi.actionSystem.ActionUpdateThread
 import com.intellij.openapi.actionSystem.AnAction
+import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.project.DumbAwareAction
 import com.intellij.openapi.util.Disposer
 import com.intellij.terminal.ui.TerminalWidget
+import com.intellij.ui.components.JBScrollPane
 import java.awt.BorderLayout
 import javax.swing.JPanel
 
@@ -12,9 +18,10 @@ class TerminalConsoleView(
     private val terminal: TerminalWidget
 ) : JPanel(BorderLayout()), ConsoleView {
     private var attachedProcessHandler: ProcessHandler? = null
+    private val scrollPane = JBScrollPane(terminal.component)
 
     init {
-        add(terminal.component, BorderLayout.CENTER)
+        add(scrollPane, BorderLayout.CENTER)
         Disposer.register(this, terminal) // cleanup
     }
 
@@ -29,7 +36,9 @@ class TerminalConsoleView(
     override fun clear() {
         terminal.sendCommandToExecute("\u001Bc")
     }
-    override fun scrollTo(offset: Int) {}
+    override fun scrollTo(offset: Int) {
+        scrollPane.verticalScrollBar.value = offset
+    }
     override fun attachToProcess(processHandler: ProcessHandler) {
         attachedProcessHandler = processHandler
     }
@@ -42,7 +51,32 @@ class TerminalConsoleView(
     override fun printHyperlink(hyperlinkText: String, info: com.intellij.execution.filters.HyperlinkInfo?) {}
     override fun getContentSize(): Int = 0
     override fun canPause(): Boolean = false
-    override fun createConsoleActions(): Array<out AnAction?> = emptyArray()
+    override fun createConsoleActions(): Array<out AnAction?> = arrayOf(MyClearConsoleAction())
     override fun allowHeavyFilters() {}
     override fun dispose() {}
+
+    // This action does not work, scrollPane.verticalScrollBar.maximum, is always te visible height and content/underlying widget is not available as of yet.
+//    private inner class ScrollToEndAction: DumbAwareAction {
+//        constructor() : super() {
+//            val message = ActionsBundle.message("action.EditorConsoleScrollToTheEnd.text")
+//            getTemplatePresentation().setDescription(message)
+//            getTemplatePresentation().setText(message)
+//            getTemplatePresentation().setIcon(AllIcons.RunConfigurations.Scroll_down)
+//        }
+//
+//        override fun getActionUpdateThread(): ActionUpdateThread = ActionUpdateThread.EDT
+//
+//        override fun actionPerformed(e: AnActionEvent) {
+//            scrollPane.verticalScrollBar.value = scrollPane.verticalScrollBar.maximum
+//        }
+//    }
+
+    private inner class MyClearConsoleAction() : DumbAwareAction(
+        ExecutionBundle.messagePointer("clear.all.from.console.action.name"),
+        ExecutionBundle.messagePointer("clear.all.from.console.action.description"), AllIcons.Actions.GC
+    ) {
+        override fun getActionUpdateThread(): ActionUpdateThread = ActionUpdateThread.EDT
+
+        override fun actionPerformed(e: AnActionEvent) = clear()
+    }
 }
